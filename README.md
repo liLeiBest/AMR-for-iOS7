b1bb7f6db52af2dd6cc1d95ff55d2cf3b1e3c14c
lilei <lilei_hapy@163.com>
2018-09-10 11:37:50 +0800

    Support Bitcode

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Compile Opencore amr 0.1.3 on iOS7
+Compile Opencore amr 0.1.3 on iOS7 Reference:[http://www.ifun.cc/blog/2014/02/09/amr-for-ios7/](http://www.ifun.cc/blog/2014/02/09/amr-for-ios7/)
 
-Reference:[http://www.ifun.cc/blog/2014/02/09/amr-for-ios7/](http://www.ifun.cc/blog/2014/02/09/amr-for-ios7/)
+Support bitcode
+
+
--- build-lib.sh
+++ build-lib.sh
@@ -2,48 +2,70 @@
 
 set -xe
 
-VERSION="0.1.3"                                                           #
-#SDKVERSION="6.1"
-SDKVERSION="7.0"
+VERSION="0.1.3"
+SDKVERSION="8.4"
+LIBSRCNAME="opencore-amr"
 
 CURRENTPATH=`pwd`
 
 mkdir -p "${CURRENTPATH}/src"
-tar zxf opencore-amr-${VERSION}.tar.gz -C "${CURRENTPATH}/src"
-cd "${CURRENTPATH}/src/opencore-amr-${VERSION}"
+tar zxvf ${LIBSRCNAME}-${VERSION}.tar.gz -C "${CURRENTPATH}/src"
+cd "${CURRENTPATH}/src/${LIBSRCNAME}-${VERSION}"
 
 DEVELOPER=`xcode-select -print-path`
-DEST="${CURRENTPATH}/opencore-amr-iphone"
+DEST="${CURRENTPATH}/lib-ios"
 mkdir -p "${DEST}"
 
-ARCHS="i386 armv7 armv7s arm64"  #armv7 armv7s
+ARCHS="armv7 arm64" #"armv7 armv7s arm64 i386 x86_64"
+# ARCHS="armv7"
 LIBS="libopencore-amrnb.a libopencore-amrwb.a"
+
+DEVELOPER=`xcode-select -print-path`
 
 for arch in $ARCHS; do
 case $arch in
 arm*)
 
-echo "Building opencore-amr for iPhone $arch ****************"
-PLATFORM="iPhoneOS"
-PATH="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/usr/bin:$PATH"
-SDK="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk"
-CC="gcc -arch $arch --sysroot=$SDK" CXX="g++ -arch $arch --sysroot=$SDK" \
-LDFLAGS="-Wl,-syslibroot,$SDK" ./configure \
---host=arm-apple-darwin --prefix=$DEST \
---disable-shared #--enable-gcc-armv7
-;;
-i386)
-echo "Building opencore-amr for iPhoneSimulator $arch*****************"
-PLATFORM="iPhoneSimulator"
-PATH="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/usr/bin:$PATH"
-SDK="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk"
-CC="gcc -arch $arch" CXX="g++ -arch $arch" \
+IOSV="-miphoneos-version-min=7.0"
+if [ $arch == "arm64" ]
+then
+IOSV="-miphoneos-version-min=7.0"
+fi
+
+echo "Building for iOS $arch ****************"
+SDKROOT="$(xcrun --sdk iphoneos --show-sdk-path)"
+CC="$(xcrun --sdk iphoneos -f clang)"
+CXX="$(xcrun --sdk iphoneos -f clang++)"
+CPP="$(xcrun -sdk iphoneos -f clang++)"
+CFLAGS="-isysroot $SDKROOT -arch $arch $IOSV -isystem $SDKROOT/usr/include -fembed-bitcode"
+CXXFLAGS=$CFLAGS
+CPPFLAGS=$CFLAGS
+export CC CXX CFLAGS CXXFLAGS CPPFLAGS
+
 ./configure \
+--host=arm-apple-darwin \
+--prefix=$DEST \
+--disable-shared #--enable-static
+;;
+*)
+IOSV="-mios-simulator-version-min=7.0"
+echo "Building for iOS $arch*****************"
+
+SDKROOT=`xcodebuild -version -sdk iphonesimulator Path`
+CC="$(xcrun -sdk iphoneos -f clang)"
+CXX="$(xcrun -sdk iphonesimulator -f clang++)"
+CPP="$(xcrun -sdk iphonesimulator -f clang++)"
+CFLAGS="-isysroot $SDKROOT -arch $arch $IOSV -isystem $SDKROOT/usr/include -fembed-bitcode"
+CXXFLAGS=$CFLAGS
+CPPFLAGS=$CFLAGS
+export CC CXX CFLAGS CXXFLAGS CPPFLAGS
+./configure \
+--host=arm-apple-darwin \
 --prefix=$DEST \
 --disable-shared
 ;;
 esac
-make -j3 > /dev/null
+make > /dev/null
 make install
 make clean
 for i in $LIBS; do
